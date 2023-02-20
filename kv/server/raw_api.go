60e5f30547c2ffcbab4f33d11421c124a7e2a488
starmilkxin <jingxin_1@163.com>
2023-02-20 13:28:19 +0800

    Project1 StandaloneKV

--- kv/server/raw_api.go
+++ kv/server/raw_api.go
@@ -2,7 +2,11 @@ package server
 
 import (
 	"context"
+
+	"github.com/pingcap-incubator/tinykv/kv/storage"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
+
+	"github.com/Connor1996/badger"
 )
 
 // The functions below are Server's Raw API. (implements TinyKvServer).
@@ -11,26 +15,102 @@ import (
 // RawGet return the corresponding Get response based on RawGetRequest's CF and Key fields
 func (server *Server) RawGet(_ context.Context, req *kvrpcpb.RawGetRequest) (*kvrpcpb.RawGetResponse, error) {
 	// Your Code Here (1).
-	return nil, nil
+	var err error
+	var reader storage.StorageReader
+	reader, err = server.storage.Reader(req.GetContext())
+	if err != nil {
+		return nil, err
+	}
+	var val []byte
+	val, err = reader.GetCF(req.GetCf(), req.GetKey())
+	if err == badger.ErrKeyNotFound {
+		err = nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	notFound := false
+	if val == nil {
+		notFound = true
+	}
+	resp := &kvrpcpb.RawGetResponse{
+		Value:    val,
+		NotFound: notFound,
+	}
+	return resp, err
 }
 
 // RawPut puts the target data into storage and returns the corresponding response
 func (server *Server) RawPut(_ context.Context, req *kvrpcpb.RawPutRequest) (*kvrpcpb.RawPutResponse, error) {
 	// Your Code Here (1).
 	// Hint: Consider using Storage.Modify to store data to be modified
-	return nil, nil
+	put := storage.Put{
+		Key:   req.GetKey(),
+		Value: req.GetValue(),
+		Cf:    req.GetCf(),
+	}
+	batch := storage.Modify{Data: put}
+	err := server.storage.Write(req.GetContext(), []storage.Modify{batch})
+	if err != nil {
+		return nil, err
+	}
+	return &kvrpcpb.RawPutResponse{}, err
 }
 
 // RawDelete delete the target data from storage and returns the corresponding response
 func (server *Server) RawDelete(_ context.Context, req *kvrpcpb.RawDeleteRequest) (*kvrpcpb.RawDeleteResponse, error) {
 	// Your Code Here (1).
 	// Hint: Consider using Storage.Modify to store data to be deleted
-	return nil, nil
+	del := storage.Delete{
+		Key: req.GetKey(),
+		Cf:  req.GetCf(),
+	}
+	batch := storage.Modify{Data: del}
+	err := server.storage.Write(req.GetContext(), []storage.Modify{batch})
+	if err == badger.ErrKeyNotFound {
+		err = nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &kvrpcpb.RawDeleteResponse{}, err
 }
 
 // RawScan scan the data starting from the start key up to limit. and return the corresponding result
 func (server *Server) RawScan(_ context.Context, req *kvrpcpb.RawScanRequest) (*kvrpcpb.RawScanResponse, error) {
 	// Your Code Here (1).
 	// Hint: Consider using reader.IterCF
-	return nil, nil
+	var err error
+	var reader storage.StorageReader
+	reader, err = server.storage.Reader(req.GetContext())
+	if err != nil {
+		return nil, err
+	}
+	iterCF := reader.IterCF(req.GetCf())
+	defer iterCF.Close()
+	var kvs []*kvrpcpb.KvPair
+	limit := req.Limit
+	for iterCF.Seek([]byte(req.GetStartKey())); iterCF.Valid(); iterCF.Next() {
+		item := iterCF.Item()
+		var key []byte
+		var val []byte
+		key = item.Key()
+		val, err = item.Value()
+		if err != nil {
+			return nil, err
+		}
+		kvPair := &kvrpcpb.KvPair{
+			Key:   key,
+			Value: val,
+		}
+		kvs = append(kvs, kvPair)
+		limit--
+		if limit == 0 {
+			break
+		}
+	}
+	rawScanResponse := &kvrpcpb.RawScanResponse{
+		Kvs: kvs,
+	}
+	return rawScanResponse, err
 }
--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -1,20 +1,35 @@
 package standalone_storage
 
 import (
+	"path"
+
 	"github.com/pingcap-incubator/tinykv/kv/config"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
+
+	"github.com/Connor1996/badger"
 )
 
 // StandAloneStorage is an implementation of `Storage` for a single-node TinyKV instance. It does not
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
 	// Your Data Here (1).
+	engines *engine_util.Engines
+	config  *config.Config
 }
 
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
 	// Your Code Here (1).
-	return nil
+	dBPath := conf.DBPath
+	kvPath := path.Join(dBPath, "kv")
+	raftPath := path.Join(dBPath, "raft")
+	kvEngine := engine_util.CreateDB(kvPath, false)
+	raftEngine := engine_util.CreateDB(raftPath, true)
+	return &StandAloneStorage{
+		engines: engine_util.NewEngines(kvEngine, raftEngine, kvPath, raftPath),
+		config:  conf,
+	}
 }
 
 func (s *StandAloneStorage) Start() error {
@@ -29,10 +44,49 @@ func (s *StandAloneStorage) Stop() error {
 
 func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
 	// Your Code Here (1).
-	return nil, nil
+	txn := s.engines.Kv.NewTransaction(false)
+	return NewStandAloneStorageReader(txn), nil
 }
 
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
 	// Your Code Here (1).
-	return nil
+	var err error
+	for _, modify := range batch {
+		switch modify.Data.(type) {
+		case storage.Put:
+			err = engine_util.PutCF(s.engines.Kv, modify.Cf(), modify.Key(), modify.Value())
+		case storage.Delete:
+			err = engine_util.DeleteCF(s.engines.Kv, modify.Cf(), modify.Key())
+		default:
+			println("StandAloneStorage Write unknown type: %s", modify.Data)
+		}
+		if err != nil {
+			break
+		}
+	}
+	return err
+}
+
+type StandAloneStorageReader struct {
+	txn *badger.Txn
+}
+
+func (s *StandAloneStorageReader) GetCF(cf string, key []byte) ([]byte, error) {
+	val, err := engine_util.GetCFFromTxn(s.txn, cf, key)
+	if err == badger.ErrKeyNotFound {
+		err = nil
+	}
+	return val, err
+}
+
+func (s *StandAloneStorageReader) IterCF(cf string) engine_util.DBIterator {
+	return engine_util.NewCFIterator(cf, s.txn)
+}
+
+func (s *StandAloneStorageReader) Close() {
+	s.txn.Discard()
+}
+
+func NewStandAloneStorageReader(txn *badger.Txn) storage.StorageReader {
+	return &StandAloneStorageReader{txn: txn}
 }
